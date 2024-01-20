5b3bcbf0eb7deec277d7d4f6266810937f682b95
Skyggebjerg <ole@skyggebjerg.dk>
2024-01-21 00:30:25 +0100

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
 #include <Arduino.h>
 
-/*
-*******************************************************************************
-* Copyright (c) 2023 by M5Stack
-*                  Equipped with M5Core sample source code
-*                          配套  M5Core 示例源代码
-* Visit for more information: https://docs.m5stack.com/en/unit/tvoc
-* 获取更多资料请访问: https://docs.m5stack.com/zh_CN/unit/tvoc
-*
-* Describe: TVOC/eCO2.
-* Date: 2021/8/26
-*******************************************************************************
-  Description: The screen will display TVOC and CO2.  屏幕将显示TVOC和CO2。
-  Note: SGP30 needs 15 seconds to initialize calibration after power on.
-SGP30开机后需要15秒进行初始校准。
-*/
+ // SGP30 needs 15 seconds to initialize calibration after power on.
 
 #include <M5Core2.h>
-
 #include "Adafruit_SGP30.h"
 
 Adafruit_SGP30 sgp;
@@ -26,9 +11,8 @@ long last_millis = 0;
 void setup() {
     M5.begin(true,true,true,true);
     M5.Lcd.setTextSize(2);
-    //M5.Lcd.setCursor(80, 0);
     M5.Lcd.drawString("TVOC TEST",110,0);
-    if (!sgp.begin()) {  // Init the sensor. 初始化传感器
+    if (!sgp.begin()) {  // Init the sensor.
         M5.Lcd.println("Sensor not found");
         while (1)
             ;
@@ -47,18 +31,15 @@ void loop() {
             M5.Lcd.drawNumber(i, 180, 40, 1);
         }
     }
-    //M5.Lcd.fillRect(0, 80, 90, 100, BLACK);
 
-    if (!sgp.IAQmeasure()) {  // Commands the sensor to take a single eCO2/VOC
-                              // measurement.  命令传感器进行一次eCO2/VOC测量
+    if (!sgp.IAQmeasure()) {  // Commands the sensor to take a single eCO2/VOC measurement.
         Serial.println("Measurement failed");
         return;
     }
-    //M5.Lcd.fillRect(100, 40, 220, 90, TFT_BLACK);
     M5.Lcd.setCursor(0, 40);
     M5.Lcd.printf("TVOC:%d ppb", sgp.TVOC);
     M5.Lcd.println("     \n"); //remove leftover characters if the next reading has less characters-OSKY
     M5.Lcd.printf("eCO2:%d ppm", sgp.eCO2);
-   M5.Lcd.println("     \n"); //remove leftover characters if the next reading has less characters-OSKY
+    M5.Lcd.println("     \n"); //remove leftover characters if the next reading has less characters-OSKY
     delay(500);
 }
